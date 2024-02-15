dfa81b62c9a26d1b7b0f40d7c667208fb0c32070
Rishi Agarwal <rishi58235@gmail.com>
2024-02-15 10:56:49 +0530

    Appointment booking added

--- IBMCAS/Controllers/PatientController.cs
+++ IBMCAS/Controllers/PatientController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using IBMCAS.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +12,29 @@ namespace IBMCAS.Controllers
     [Authorize(Roles = "PATIENT")]
     public class PatientController : Controller
     {
+        Models.IBMCASDBEntities1 _db = new Models.IBMCASDBEntities1();
         // GET: Patient
         public ActionResult Index()
         {
+            return View(_db.Appointments.ToList());
+        }
+
+        public ActionResult BookAppointment()
+        {
+            ViewBag.PhysicianList = _db.Physicians.ToList();
             return View();
+        }
+        [HttpPost]
+        public ActionResult BookAppointment([Bind(Include = "PhysicianID, DateRequested" )] Appointment appointment)
+        {
+            appointment.PatientID = int.Parse(Session["ReferenceID"].ToString());
+            appointment.isVisited = 0;
+            appointment.DateCreated = DateTime.Now.Date;
+            appointment.AppointmentToken = DateTime.Now.Year.ToString() + DateTime.Now.TimeOfDay.ToString("hhmmss");
+            _db.Appointments.Add(appointment);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
